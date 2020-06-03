cccc27fc5e61921a033af352fe2240fbf0a0616e
annaptasznik <annaptasznik@users.noreply.github.com>
2020-06-03 00:42:09 -0700

    Clean up unused code and files

--- app/src/main/java/edu/uw/eep523/mapslocation/Constants.kt
+++
@@ -1,9 +0,0 @@
-package edu.uw.eep523.mapslocation
-
-object Constants{
-    const val DELAY_SPLASH = 1000L
-    const val ROUTE_DATA = "route_data"
-    const val ADD_SCREEN = "add_screen"
-    const val UPDATE_SCREEN = "update_screen"
-    const val ACTION = "action"
-}

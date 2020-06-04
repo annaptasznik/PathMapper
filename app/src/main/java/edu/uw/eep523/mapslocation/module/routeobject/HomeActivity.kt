3dbd76883ff57e3f17dbdd6512310fb7df67b491
annaptasznik <annaptasznik@users.noreply.github.com>
2020-06-03 21:23:37 -0700

    Make dummy graph

--- app/src/main/java/edu/uw/eep523/mapslocation/module/routeobject/HomeActivity.kt
+++ app/src/main/java/edu/uw/eep523/mapslocation/module/routeobject/HomeActivity.kt
@@ -1,15 +1,23 @@
 package edu.uw.eep523.mapslocation.module.routeobject
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import edu.uw.eep523.mapslocation.*
+import com.jjoe64.graphview.GraphView
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.LineGraphSeries
+import edu.uw.eep523.mapslocation.MapsLayersActivity
+import edu.uw.eep523.mapslocation.OnItemClickListener
+import edu.uw.eep523.mapslocation.R
+import edu.uw.eep523.mapslocation.RouteDatabaseAdapter
 import kotlinx.android.synthetic.main.activity_home.*
-import kotlinx.android.synthetic.main.toolbar.*
+import java.util.*
+import kotlin.collections.ArrayList
+
 
 class HomeActivity : AppCompatActivity(),View.OnClickListener, OnItemClickListener {
     private lateinit var databaseHelper: RouteDatabaseAdapter
@@ -19,6 +27,18 @@ class HomeActivity : AppCompatActivity(),View.OnClickListener, OnItemClickListen
     private var GrouteDate= ""
     private var GrouteCategory= ""
     private var GrouteDistance= ""
+
+
+    private var mNumLabels = 0
+
+    fun Dates(numLabels: Int) {
+        mNumLabels = numLabels
+    }
+
+    fun Dates() {
+        mNumLabels = 3
+    }
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -59,6 +79,7 @@ class HomeActivity : AppCompatActivity(),View.OnClickListener, OnItemClickListen
         ///
 
 
+
         initView()
     }
 
@@ -68,8 +89,7 @@ class HomeActivity : AppCompatActivity(),View.OnClickListener, OnItemClickListen
      * This method is used to set views and clicklistener
      */
     private fun initView() {
-        backIv.setImageResource(R.drawable.menu_icon)
-        titleTv.setText(getString(R.string.route_title))
+
         setClickListener()
 
         databaseHelper = RouteDatabaseAdapter(this)
@@ -77,8 +97,6 @@ class HomeActivity : AppCompatActivity(),View.OnClickListener, OnItemClickListen
         routeDataObjectAdapter.setOnClickListener(this)
         routeRecylerview.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
         routeRecylerview.adapter = routeDataObjectAdapter
-
-
 
     }
 
@@ -96,7 +114,72 @@ class HomeActivity : AppCompatActivity(),View.OnClickListener, OnItemClickListen
     private fun getDataFromDb() {
         routeList.clear()
         routeList.addAll( databaseHelper.getAllData())
+
+        for(item in routeList){
+            Log.e("item", item.routeFilename.toString())
+        }
+
+        launchGraph()
         showData()
+    }
+
+
+    private fun launchGraph(){
+
+        val date:Date = Date(2020,3,1)
+        val date2:Date = Date(2020,3,4)
+        val date3:Date = Date(2020,3,7)
+
+        val graph = findViewById(R.id.graph) as GraphView
+        val series: LineGraphSeries<DataPoint> = LineGraphSeries<DataPoint>(
+            arrayOf<DataPoint>(
+                DataPoint(date, 1.0),
+                DataPoint(date2, 5.0),
+                DataPoint(date3, 3.0)
+            )
+        )
+
+        val series2: LineGraphSeries<DataPoint> = LineGraphSeries<DataPoint>(
+            arrayOf<DataPoint>(
+                DataPoint(date, 9.0),
+                DataPoint(date2, 6.0),
+                DataPoint(date3, 7.0)
+            )
+        )
+        graph.addSeries(series)
+
+        graph.addSeries(series2)
+
+        series.setTitle("Cycle")
+        series2.setTitle("Run")
+        series.color = Color.RED
+
+        // styling legend
+        graph.legendRenderer.isVisible = true
+        graph.legendRenderer.textSize = 25f
+        graph.legendRenderer.backgroundColor = Color.argb(150, 50, 0, 0)
+        graph.legendRenderer.textColor = Color.WHITE
+        graph.legendRenderer.setFixedPosition(500,195)
+        //graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
+        //graph.getLegendRenderer().setMargin(30);
+
+        // titles
+        graph.title = "Distance by Day"
+        graph.gridLabelRenderer.verticalAxisTitle = "km"
+        graph.gridLabelRenderer.horizontalAxisTitle = "Date"
+        graph.gridLabelRenderer.isHorizontalLabelsVisible = false
+
+        // axes
+        /*
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(130);
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinY(-115);
+        graph.getViewport().setMaxY(0);
+        graph.getViewport().setYAxisBoundsManual(true);
+         */
+
+
     }
 
     /**
